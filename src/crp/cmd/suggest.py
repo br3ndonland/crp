3c673c219869f3be2fb4a88dc533ebf93bb7a2fa
Brendon Smith <bws@bws.bio>
2025-09-08 00:26:24 -0400

    Enforce minimum and maximum image dimensions
    This commit will add enforcement of minimum and maximum image dimensions
    to the `suggest` subcommand.
    
    - Backdrops (16:9): minimum 1280x720 pixels, maximum 3840x2160 pixels
    - Posters (2:3): minimum 500x750 pixels, maximum 2000x3000 pixels
    
    This will be accomplished with a dataclass that checks image dimensions
    as part of its `__init__` method. Tests will be added to verify that the
    error messages and exit codes are propagated back to the user.

--- src/crp/cmd/suggest.py
+++ src/crp/cmd/suggest.py
@@ -1,28 +1,20 @@
 import click
 
-from crp.types import AspectRatio, ImageType
+from crp.types import Image, ImageType
 
 
-def suggest_aspect_ratio(
-    image_type: ImageType, *, width: int, height: int
-) -> AspectRatio:
-    """Suggest a width and height to use for cropping the given image type."""
-    if image_type is ImageType.BACKDROP:
-        aspect_ratio = AspectRatio(16, 9)
-        # TODO: enforce minimum and maximum dimensions
-        # https://www.themoviedb.org/bible/image
-        # maximum_size = AspectRatio(3840, 2160)
-        # minimum_size = AspectRatio(1280, 720)
-    elif image_type is ImageType.POSTER:
-        aspect_ratio = AspectRatio(2, 3)
-        # TODO: enforce minimum and maximum dimensions
-        # https://www.themoviedb.org/bible/image
-        # maximum_size = AspectRatio(2000, 3000)
-        # minimum_size = AspectRatio(500, 750)
-    scale = min(width / aspect_ratio.width, height / aspect_ratio.height)
-    suggested_width = int(aspect_ratio.width * scale)
-    suggested_height = int(aspect_ratio.height * scale)
-    return AspectRatio(width=suggested_width, height=suggested_height)
+def suggest_dimensions(image: Image) -> Image:
+    """Suggest dimensions to use for cropping the given image type."""
+    scale = min(
+        image.width / image.aspect_ratio.width, image.height / image.aspect_ratio.height
+    )
+    scaled_width = int(image.aspect_ratio.width * scale)
+    scaled_height = int(image.aspect_ratio.height * scale)
+    suggested_width = max(image.minimum_width, min(scaled_width, image.maximum_width))
+    suggested_height = max(
+        image.minimum_height, min(scaled_height, image.maximum_height)
+    )
+    return Image(image.image_type, width=suggested_width, height=suggested_height)
 
 
 @click.command()
@@ -33,24 +25,29 @@ def suggest(image_type: ImageType, width: int, height: int) -> None:
     """Suggest dimensions for cropping images of the given image type.
 
     Images often need to be cropped to specific aspect ratios and dimensions for upload
-    to sites like TheMovieDB (https://www.themoviedb.org/bible/image).
+    to sites like TMDB. This command suggests dimensions to use for cropping. Dimensions
+    conform to [TMDB guidelines](https://www.themoviedb.org/bible/image).
 
     \b
-    Backdrops: 16:9 (minimum 1280x720 pixels, maximum 3840x2160 pixels)
-    Posters: 2:3 (minimum 500x750 pixels, maximum 2000x3000 pixels)
+    Backdrops (16:9): minimum 1280x720 pixels, maximum 3840x2160 pixels
+    Posters (2:3): minimum 500x750 pixels, maximum 2000x3000 pixels
 
-    This command suggests dimensions to use for cropping. Examples:
+    Examples:
 
     \b
     crp suggest --width=3940 --height 2160 backdrop -> Crop to 3840x2160
     crp suggest --width 1652 --height 2214 poster -> Crop to 1476x2214
-
-    `-h` is not used as a short option for `--height` because it would conflict
-    with the `-h` used for help (https://github.com/pallets/click/issues/2819).
-
-    This command does not currently enforce minimum and maximum dimensions. Support for
-    minimum and maximum dimensions is planned for a future release.
     """
-    # TODO: enforce minimum and maximum dimensions
-    aspect_ratio = suggest_aspect_ratio(image_type, width=width, height=height)
-    click.secho(f"Crop to {aspect_ratio.width}x{aspect_ratio.height}", bold=True)
+    # `-h` is not used as a short option for `--height` because it would conflict
+    # with the `-h` used for help (https://github.com/pallets/click/issues/2819).
+    try:
+        input_image = Image(image_type, width=width, height=height)
+        suggested_image = suggest_dimensions(input_image)
+        message = (
+            f"Crop {input_image.image_type} to {suggested_image.dimensions} "
+            f"({suggested_image.aspect_ratio})."
+        )
+        click.secho(message, bold=True)
+    except Exception as e:
+        error_message = click.style(str(e), fg="red")
+        raise click.UsageError(error_message) from e
